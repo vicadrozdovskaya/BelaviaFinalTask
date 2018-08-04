c34fa90033346173721f1ec9ae7544c23815eab5
Victoria <vica.drozdovskaya@gmail.com>
2018-08-04 18:32:42 +0300

    refactoring of test function

--- src/test/java/by/htp/drozdovskaya/automation/belavia/MainPageTest.java
+++ src/test/java/by/htp/drozdovskaya/automation/belavia/MainPageTest.java
@@ -1,37 +1,48 @@
 package by.htp.drozdovskaya.automation.belavia;
 
-import org.testng.Assert;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import by.htp.drozdovskaya.automation.belavia.entity.Ticket;
+import by.htp.drozdovskaya.automation.belavia.entity.comparator.TicketComporatorCost;
+import by.htp.drozdovskaya.automation.belavia.entity.comparator.TicketComporatorDate;
 import by.htp.drozdovskaya.automation.belavia.steps.StepsMainPage;
 
-
 public class MainPageTest {
-	
+
 	private StepsMainPage steps;
 	private final String CITY_FROM = "МИНСК";
 	private final String CITY_TO = "РИГА";
+	private final static int MONTH_RANGE = 3;
 
 	@BeforeMethod(description = "Init browser")
-	public void setUp()
-	{
+	public void setUp() {
 		steps = new StepsMainPage();
 		steps.initBrowser();
 	}
 
 	@Test(description = "init from City")
-	public void initCity()
-	{
-		steps.initFromCity(CITY_FROM,CITY_TO);
-		
-	
+	public void initCity() {
+		Calendar endDate = new GregorianCalendar();
+		endDate.add(Calendar.MONTH, MONTH_RANGE);
+		steps.initDateAndDestination(CITY_FROM, CITY_TO);
+		List<Ticket> tickets = steps.getAvailibleTickets(endDate);
+		Collections.sort(tickets, new TicketComporatorCost());
+		System.out.println("Отсортированный по стоимости ");
+		System.out.println(tickets);
+		Collections.sort(tickets, new TicketComporatorDate());
+		System.out.println("Отсортированный по дате вылета ");
+		System.out.println(tickets);
 	}
 
 	@AfterMethod(description = "Stop Browser")
-	public void stopBrowser()
-	{
+	public void stopBrowser() {
 		steps.closeDriver();
 	}
 
